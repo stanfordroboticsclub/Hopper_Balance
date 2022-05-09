51c0c4af91dbebc6dc0b097c59ef8ab5bd534742
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-05-08 22:05:40 -0700

    hopper manual control send to serial

--- python/hopper_manual_control.py
+++ python/hopper_manual_control.py
@@ -1,22 +1,52 @@
 import numpy as np
 import time
+import struct
 
 import hid
 import serial
+from serial.tools import list_ports
 
 LINEAR_VELOCITY_SCALE = 10.0
 ANGULAR_VELOCITY_SCALE = 10.0
+
+BAUDRATE = 115200
+
+class SerialSender:
+    def __init__(self, port, baudrate, start='H ', verbose=False):
+        self.port = port
+        self.baudrate = baudrate
+        self.start = start
+        self.verbose = verbose
+
+        self.ser = serial.Serial(port, baudrate)
+        
+    def send(self, linear_velocity, angular_velocity):
+        data = struct.pack("<cff", self.start, linear_velocity, angular_velocity)
+        self.ser.write(data)
+        
+    def open(self):
+        self.ser.open()
+    
+    def close(self):
+        self.ser.close()
+
+    def read_byte(self):
+        return self.ser.read()
+
+    def read_float(self):
+        return struct.unpack("<f", self.ser.read(4))[0]
+
+    def read_int(self):
+        return struct.unpack("<i", self.ser.read(4))[0]
 
 class JoystickInterface:
     def __init__(self):
         self.gamepad = hid.device()
         for device in hid.enumerate():
-            if (device['product_string'] == 'FrSky Simulator') or (device['product_string'] == 'BetaFPV Taranis Joystick'):
+            if (device['product_string'] == 'FrSky Simulator') or (device['product_string'] == 'BetaFPV Taranis Joystick') or (device["vendor_id"] == 1155):
                 self.gamepad.open(device['vendor_id'], device['product_id'])
                 break
         self.gamepad.set_nonblocking(True)
-        while True:
-            self.get_command()
 
     def get_command(self):
         report = self.gamepad.read(64)
@@ -24,7 +54,25 @@ class JoystickInterface:
             event = [r - 256 if r > 127 else r for r in report]
             command_linear_vel = LINEAR_VELOCITY_SCALE * event[4] / 127
             command_angular_vel = ANGULAR_VELOCITY_SCALE * event[6] / 127
-            print(command_linear_vel, command_angular_vel)
-            return
+            return command_linear_vel, command_angular_vel
 
-JoystickInterface()
+
+if __name__ == "__main__":
+    
+    try: 
+        port = next(list_ports.grep(".*usbmodem.*"))
+    except:
+        try:
+            port = next(list_ports.grep(".*tty.*"))
+        except:
+            raise RuntimeError("No port found")
+
+    ser = SerialSender(port.device, BAUDRATE)
+    joy = JoystickInterface()
+
+    while True:
+        lin, ang = joy.get_command()
+        ser.send(lin, ang)
+        print(ser.read_byte())
+        print(ser.read_float())
+        print(ser.read_float())
