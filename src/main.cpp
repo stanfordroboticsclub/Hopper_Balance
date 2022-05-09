44890fe58f9d31999ab527d5203bfd017e48481d
CrazyAZ <aaron@hschultz.net>
2022-05-09 10:54:02 -0700

    Updated communication protocol for teleop

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,34 @@ uint32_t last_control_step_;
 float des_state_[6] = {0, 0, 0, 0, 0, 0};
 float des_vel_ = 0;
 float des_yaw_rate_ = 0;
-const float kMaxYawRate = 1000;
 
-Filter velFilter(2, kControlFrequency);
-Filter yawFilter(2, kControlFrequency);
+Filter velFilter(5, kControlFrequency);
+Filter yawFilter(5, kControlFrequency);
+
+#define MESSAGE_LENGTH 2 * sizeof(float)
+#define SENTINAL 'H'
+
+void readCommands() {
+  static char buffer[MESSAGE_LENGTH];
+  if (Serial.available() > MESSAGE_LENGTH) {
+    if (Serial.read() == SENTINAL) {
+      for (size_t i = 0; i < MESSAGE_LENGTH; i++) {
+        buffer[i] = Serial.read();
+      }
+      des_vel_ = ((float *) buffer)[0];
+      des_yaw_rate_ = ((float *) buffer)[1];
+    } else {
+      while (Serial.available() > 0) {
+        Serial.read();
+      }
+    }
+  }
+}
 
 void setup(){
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
   Serial.begin(115200);
-  Serial.setTimeout(1);
   while (!Serial) {}
   delay(5000);
 
@@ -33,25 +51,30 @@ void setup(){
 }
 
 void loop(){
-  if (Serial.available() > 0) {
-    des_yaw_rate_ = constrain(Serial.parseFloat(), -kMaxYawRate, kMaxYawRate);
-    Serial.read();
-    Serial.println(des_yaw_rate_);
-  }
+  readCommands();
 
   robot_.PollCAN();
   if(micros() - last_control_step_ > (uint32_t) (1000000 / kControlFrequency)) {
+    last_control_step_ = micros();
+
     if (robot_.get_state() == IDLE) {
       des_state_[1] = 0.0;
       des_state_[4] = 0.0;
       velFilter.reset();
       yawFilter.reset();
     } else {
+      float vel = velFilter.filter(des_vel_);
       float yaw_rate = yawFilter.filter(des_yaw_rate_);
+
+      des_state_[3] = vel;
       des_state_[5] = yaw_rate;
+
+      des_state_[1] += vel / kControlFrequency;
       des_state_[4] += yaw_rate / kControlFrequency;
     }
-    last_control_step_ = micros();
+    
     robot_.control_step(des_state_);
   }
 }
+
+
